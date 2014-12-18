d521b99375544bee5d7336903e4e418ddb0b00cf
Rubiromi <hiromi.deyoung@gmail.com>
2014-12-18 13:47:32 -0700

    trying to fix animation on #intro issue.

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $('#intro').on('mouseenter', function() {
-    // $('#intro').css({'color': '#8FE8F2'}, 4000);
-    $(‘#intro’).animate({color: “#8FE8F2”}, 4000);
+  $('#intro').on('mouseover', function() {
+    $('#intro').css({'color': '#8FE8F2'}, 4000);
+    // $('#intro').animate({'color': '#8FE8F2'}, 'slow');
   });
   $('#photo01').fadeIn(3400);
 });
