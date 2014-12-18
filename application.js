ee3429344cbc9a4d0eed0af3a9ffd1b06ae12ba1
Rubiromi <hiromi.deyoung@gmail.com>
2014-12-18 12:15:24 -0700

    added new line on .js to control animation speed to change font color.  Added an image.

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('#intro').on('mouseenter', function() {
-    $('#intro').css({'color': '#8FE8F2'}, "slow");
+    // $('#intro').css({'color': '#8FE8F2'}, 4000);
+    $(‘#intro’).animate({color: “#8FE8F2”}, 4000);
   });
-  $('#photo01').fadeIn(3700);
+  $('#photo01').fadeIn(3400);
 });
