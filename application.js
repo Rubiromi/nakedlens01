266998f8099fdf7c059d4ad45510c8b601bcd412
Rubiromi <hiromi.deyoung@gmail.com>
2014-12-19 12:42:49 -0700

    Added pricing page with jumbotron.

--- application.js
+++ application.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $('#intro').on('mouseover', function() {
     $('#intro').css({'color': '#8FE8F2'}, 4000);
     // $('#intro').animate({'color': '#8FE8F2'}, 'slow');
