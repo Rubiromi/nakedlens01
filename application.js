6da28fd94f32d7ddcd08444660f739e0aebef8fd
Rubiromi <hiromi.deyoung@gmail.com>
2014-12-19 11:36:31 -0700

    Added nav samples. Fixed layout.

--- application.js
+++ application.js
@@ -3,20 +3,27 @@ $(document).ready(function(){
     $('#intro').css({'color': '#8FE8F2'}, 4000);
     // $('#intro').animate({'color': '#8FE8F2'}, 'slow');
   });
-  
+
   $('#intro-jp').on('mouseover', function() {
     $('#intro-jp').css({'color': '#8FE8F2'}, 4000);
   });
 
-  $('#footer-border').on('mouseenter', 'p', function () {
-    $(this).animate({'font-size': '25px',
-                     'margin': '-35px',
-                     'left': '25%'}, 600)
+  $('#intro-jp2').on('mouseover', function() {
+    $('#intro-jp2').css({'color': '#F5FCA7'}, 4000);
+  });
+
+  $('#intro-jp3').on('mouseover', function() {
+    $('#intro-jp3').css({'color': '#8FE8F2'}, 4000);
+  });
+
+  $('#contact').on('click', function () {
+    $('#footer-border').animate({'font-size': '25px',
+                     'position-left': '-15%'}, 600)
            .css({'color': '#8FE8F2'});
   });
 
-  $('#footer-border').on('mouseleave', 'p', function () {
-    $(this).animate({'position': 'fixed',
+  $('#contact').on('mouseleave', function () {
+    $('#footer-border').animate({'position': 'fixed',
                      'left': '34%',
                      'font-weight': 300,
                      'font-size': '15px',
